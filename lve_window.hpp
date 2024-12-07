6d46549d30757d6a17303d74210645cbe3aa52ae
rrozdoce <felipefelipevilhalva@gmail.com>
2024-12-07 19:34:09 -0400

    .

--- lve_window.hpp
+++ lve_window.hpp
@@ -8,10 +8,13 @@ namespace lve {
    class LveWindow
    {
     public:
-       LveWindow(int w, int h, std::string name);
-       ~LveWindow();
+      LveWindow(int w, int h, std::string name);
+      ~LveWindow();
 
-       bool shouldClose(){ return glfwWindowShouldClose(window); }
+      LveWindow(const LveWindow &) = delete;
+      LveWindow &operator=(const LveWindow &) = delete;
+
+      bool shouldClose(){ return glfwWindowShouldClose(window); }
 
     private:
        void initWindow();
